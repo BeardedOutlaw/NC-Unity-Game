6a780b63572da0794b4ca2c9ead973acd4439d3b
Matty Simpson <beardedoutlaw37@outlook.com>
2018-02-26 11:34:41 +0000

    Practive Player Code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string logText = "Hello World Again";
+	public float speed = 2;
+	// Use this for initialization
+	void Start () {
+		Debug.Log(logText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(logText);
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+		rigidbody.velocity = Vector2.right * speed;
+	}
+}
